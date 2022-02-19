fc4d169b34a0f0620a10ce8ef7ac12ae377ea351
Celso Bastos <celsorico@gmail.com>
2022-02-18 23:19:05 -0300

    js

---
+++ public/js/conceitos.js
@@ -0,0 +1,21 @@
+"use strict"
+/*
+Tipos de dados
+--------------------------------------
+number
+string
+boolean
+null
+undefined
+object
+*/
+
+let nome = '';
+
+let aceitoContrato = false;
+
+let nomeCompleto = "Jonas Almeida";
+const COLOR_RED = '#ff0000';
+
+console.log(typeof aceitoContrato);
+console.log(nome);
---
+++ public/js/hello-world.js
@@ -0,0 +1,10 @@
+console.log('meu primeiro script');
+
+var nome = 'Paulo';
+
+if (nome !== '') {
+    console.log('Nome paulo não existe');
+}
+else {
+    console.log('Olá ' + nome);
+}
