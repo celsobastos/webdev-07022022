78fbfd3e2f4b439f353f7557b42f099633ef1dc1
Celso Bastos <celsorico@gmail.com>
2022-02-21 22:56:26 -0300

    javascript

--- public/js/conceitos.js
+++ public/js/conceitos.js
@@ -17,24 +17,53 @@ object
 // let convertBool = String(false);
 // console.log(convertBool);
 
-let notaPrimeiroBimestre = +'10'; // " + - * / % (mod) resto"
-let notaSegundoBimestre = 5;
+let notaPrimeiroBimestre = [10, 5, 0, 4, 9]; // " + - * / % (mod) resto"
+let notaSegundoBimestre  = [10, 5, 0, 8, 6];
 let faltas = 0;
-let media = (notaPrimeiroBimestre + notaSegundoBimestre) / 2;
-let aluno = "Carlos";
+let aluno = ['Pedro','Paulo','Jonas','Amanda','Bianca']; // Array
+
 /*
     se media >= 7 e faltas = 0 => aprovado
     se media < 7 reprovado
     >, >=, <=, !=, ==, === , !== (e ou não)
 */
-if (media >= 7 && faltas === 0) {
-    console.log('Olá ' + aluno + ' a media é: ' + media + ' você está aprovada.');
-}
-else{
-    console.log('Olá ' + aluno + ' a media é: ' + media + ' Você está Reprovado(a)');
+for(let i = 0; i < aluno.length; i++){
+    let media = (notaPrimeiroBimestre[i] + notaSegundoBimestre[i]) / 2;
+    let mensagem;
+    let nomeDoAluno = aluno[i];
+    if (media >= 7 && faltas === 0) {
+        mensagem = `Olá ${nomeDoAluno} a media é: ${media} você está aprovada`;
+    }
+    else if (media > 3 && faltas === 0){
+
+        mensagem = `Olá ${nomeDoAluno} a media é: ${media} você está de Recuperação`;
+    }
+    else{
+        mensagem = `Olá ${nomeDoAluno} a media é: ${media} você está Reprovado(a)`;
+    }
+
+    console.log(mensagem);
 }
 
 
+
+/*
+let i = 1;
+while (i <= 10) {
+
+    // if(i === 5) {
+    //     break;
+    // }
+
+    if (media >= 7 && faltas === 0) {
+        console.log('Olá ' + aluno + ' a media é: ' + media + ' você está aprovada.');
+    }
+    else{
+        console.log('Olá ' + aluno + ' a media é: ' + media + ' Você está Reprovado(a)');
+    }
+    i++; // "i = i + 2" // "i-- == i = i - 1"
+}
+*/
 
 
 
